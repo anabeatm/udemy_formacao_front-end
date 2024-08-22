213cf67303c5a9c1acc312a2e65148d479e09542
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2024-08-22 17:29:48 -0300

    att 22-08

--- aulas/javascript/funcoes/js/script.js
+++ aulas/javascript/funcoes/js/script.js
@@ -146,3 +146,64 @@
 
 // repeatText("Testando.")
 // repeatText("5 vezes.", 5)
+
+// Aula 9 - closure
+// function someFunction() {
+//   let txt = "Alguma coisa";
+
+//   function display() {
+//     console.log(txt);
+//   }
+
+//   display();
+// }
+
+// someFunction();
+
+// Aula 10 - mais sobre closure
+// const multiplicationClosure = (n) => {
+//   return (m) => {
+//     return m * n;
+//   };
+// };
+
+// const c1 = multiplicationClosure(5);
+
+// const c2 = multiplicationClosure(10);
+
+// console.log(c1);
+
+// console.log(c2);
+
+// console.log(c1(5));
+
+// console.log(c2(10));
+
+// Aula 11 - recursão
+// const untilTen = (n, m) => {
+//   if (n < 10) {
+//     console.log("A função parou de executar!");
+//   } else {
+//     const x = n - m;
+
+//     console.log(x);
+
+//     untilTen(x, m);
+//   }
+// };
+
+// untilTen(100, 7);
+
+function factorial(x) {
+  if (x == 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+
+const num = 6;
+
+const result = factorial(num);
+
+console.log(`O fatorial do número ${num} é ${result}.`);
