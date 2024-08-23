e5f04c1a9253d31519f42ca093bddef4fc3a9eed
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2024-08-23 17:13:27 -0300

    att 23-08

---
+++ aulas/javascript/arrays&objetos/script.js
@@ -0,0 +1,25 @@
+// aula 1 - arrays
+// const lista = [1, 2, 3, 4, 5]
+
+// console.log(lista)
+
+// console.log(typeof(lista))
+
+// const itens = ["Nomes", true, 2, 4.12, []] // pode ter vários tipos de dados
+
+// console.log(itens)
+
+// OBS:
+const arr = ["a", "b", "c"] // 0 based
+//            0    1    2
+
+console.log(arr[0]) // -> saída: a
+
+// aula 2 - propriedades
+const numbers = [5, 4, 3];
+console.log(numbers.length);
+
+console.log(numbers["length"]);
+
+const myName = "Matheus";
+console.log(myName.length);
