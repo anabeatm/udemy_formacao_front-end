dcfbf8d0833a61c44bcca6ad30657f754e1b087b
Ana Beatriz Tavares Malaquais <ana.beat20051@gmail.com>
2024-08-24 16:47:03 -0300

    att 24-08

--- aulas/javascript/arrays&objetos/script.js
+++ aulas/javascript/arrays&objetos/script.js
@@ -10,16 +10,43 @@
 // console.log(itens)
 
 // OBS:
-const arr = ["a", "b", "c"] // 0 based
+const arr = ["a", "b", "c"]; // 0 based
 //            0    1    2
 
-console.log(arr[0]) // -> saída: a
+console.log(arr[0]); // -> saída: a
 
 // aula 2 - propriedades
-const numbers = [5, 4, 3];
-console.log(numbers.length);
+// const numbers = [5, 4, 3];
+// console.log(numbers.length);
 
-console.log(numbers["length"]);
+// console.log(numbers["length"]);
 
-const myName = "Matheus";
-console.log(myName.length);
+// const myName = "Matheus";
+// console.log(myName.length);
+
+// aula 3 - métodos
+// const otherNumbers = [1, 2, 3];
+
+// const allNumbers = [4, 5, 6].concat(otherNumbers);
+
+// console.log(allNumbers);
+
+// const text = "algum texto";
+// console.log(text.toUpperCase());
+
+// console.log(typeof text.toUpperCase);
+
+// console.log(text.indexOf("g"));
+
+// aula 4 - object literals
+// const person = {
+//   name: "Matheus",
+//   age: 40,
+//   job: "programador",
+// };
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person.name.length);
+
+// aula 5 - criando e deletando propriedades
